ee76534e12906d8a2824d56dbd8400483fd02b27
Scott Swarthout <scott.swarthout724@gmail.com>
2016-04-30 20:21:58 -0400

    changed var name in message methods

--- api/messages/methods.ts
+++ api/messages/methods.ts
@@ -1,14 +1,14 @@
 import {MessageInterface, Messages} from "./messages";
 Meteor.methods({
-    'messages.addMessage': function (text:string, channel: string) {
-        let channel:MessageInterface = <MessageInterface>
+    'messages.addMessage': function (text:string, channel:string) {
+        let message: MessageInterface = <MessageInterface>
         {
             content: text,
             timestamp: new Date(),
             channelId: channel,
             authorId: this.userId
         };
-        Messages.insert(channel);
+        Messages.insert(message);
     },
 
     'messages.deleteMessage': function (messageId:string) {
